ff8b6c19406ec5815efebdf2ce2f0cd2f65f1ea4
kevin.lee@refreshlabs.co <kevin.lee@lynkr.xyz>
2017-11-15 17:52:15 -0800

    experiment

--- client/src/utils/testApi.js
+++ client/src/utils/testApi.js
@@ -1,11 +1,9 @@
 var axios = require('axios');
 
-var test_uri = process.env.TEST_URI + '/test/gavin' || 'http://localhost:8080/test/gavin';
-
 module.exports = {
 
     fetchName: function() {
-        return axios.get(test_uri)
+        return axios.get("/test/gavin")
             .then(function(response) {
                 return response.data.user;
             }) 
