d703e0f21295200b63103c9cc2de04a35b6dd20b
Gavin Berkowitz <gavin.berkowitz.bu@gmail.com>
2017-11-24 10:39:51 -0500

    Simplify Quadrant layout and play with colors

--- client/src/layouts/Quadrant/BlankQuadrant.js
+++ client/src/layouts/Quadrant/BlankQuadrant.js
@@ -3,7 +3,7 @@ import React from 'react'
 import './quadrant.css'
 var BlankQuadrant = function(props){
     
-    return <div className="quadrantItem"></div>
+    return <div className={props.layoutClass}></div>
 
 }
 
