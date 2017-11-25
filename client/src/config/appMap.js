3c0079049c2b9e4d39c709ed7fcf8e0a5c5e7ec9
Gavin Berkowitz <gavin.berkowitz.bu@gmail.com>
2017-11-25 10:31:24 -0500

    Defer load component in appMap for maximum sucess!

--- client/src/config/appMap.js
+++ client/src/config/appMap.js
@@ -1,30 +1,20 @@
-//Why doesn't importing these from containers/index.js work?
-import TwentyOneContainer from '../containers/TwentyOne/TwentyOneContainer'
-import GdaxContainer from '../containers/Gdax/GdaxContainer'
-
+import {TwentyOneContainer, GdaxContainer}  from '../containers/'
 
 const _appMap = {
-    "blackjack": TwentyOneContainer,
-    "gdax": GdaxContainer
-}
-
-var getAppFromName = function(name){
-    return _appMap[name];
+    "blackjack": () => TwentyOneContainer,
+    "gdax": () => GdaxContainer
 }
 
 var getOpenApps = function(apps){
     var openApps = []
-    var keys = Object.keys(apps);
-    keys.map(function(key){
-        openApps.push(_appMap[key])
+    Object.keys(apps).map(function(key){
+        return openApps.push(_appMap[key]());
     });
     return openApps;
 }
 
 const AppMap = {
-    getAppFromName: getAppFromName,
-    getOpenApps: getOpenApps,
-    map: _appMap
+    getOpenApps: getOpenApps
 }
 
 export default AppMap
--- client/src/index.js
+++ client/src/index.js
@@ -13,6 +13,7 @@ import registerServiceWorker from './registerServiceWorker';
 
 import './index.css';
 import * as reducers from './redux/modules/';
+import * as containers from './containers/index';
 
 const store = createStore(
     combineReducers(reducers), 
