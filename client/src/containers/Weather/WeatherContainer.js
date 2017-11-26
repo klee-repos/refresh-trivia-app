380e2aa31089012fe258b1dfaa8249d8afcceeba
Gavin Berkowitz <gavin.berkowitz.bu@gmail.com>
2017-11-26 10:21:14 -0500

    Create weather app files and folders

---
+++ client/src/containers/Weather/WeatherContainer.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+import Weather from '../../components'
+
+class WeatherContainer extends Component{
+    constructor(props){
+        super(props);
+
+
+    }
+    
+    render(){
+        return <Weather />
+    }
+}
