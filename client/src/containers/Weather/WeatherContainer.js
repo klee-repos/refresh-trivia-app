d037dbc41cc2ad784a1207a8b7066c900548757a
Gavin Berkowitz <gavin.berkowitz.bu@gmail.com>
2017-11-26 15:59:46 -0500

    Make an API call from client when weather is mounted

--- client/src/containers/Weather/WeatherContainer.js
+++ client/src/containers/Weather/WeatherContainer.js
@@ -1,18 +1,34 @@
 import React, {Component} from 'react'
 
 import Weather from '../../components'
+import axios from 'axios'
 
 class WeatherContainer extends Component{
 
     constructor(props){
         super(props);
 
-        
+        this.state = {
+            weatherData: {}
+        }
     }
+
+    componentDidMount(){
+        this.Today();
+    }
+
+    Today(){
+        axios.get('/apps/weather/forecast/today?' + "lat=" + this.props.location.lat +"&lon=" + this.props.location.lon)
+            .then(function(data){
+                this.setState({weatherData:data})
+            })
+    }
+
     render(){
         return (
             <div className={this.props.layoutClass}>
-                <Weather />
+                {/* <Weather weatherData={this.state.weatherData}/> */}
+                {this.state.weatherData.timeZone}
             </div>
         )
     }
