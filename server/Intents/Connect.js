78e251394ad7e6a7f3322eae76abe28cfce6a699
Gavin Berkowitz <gavin.berkowitz.bu@gmail.com>
2018-01-03 21:30:23 -0500

    commit

--- server/Intents/Connect.js
+++ server/Intents/Connect.js
@@ -21,7 +21,6 @@ var execute = function(args, assistant){
                 .finish();
         })
         .catch(function(err){
-            console.log
             assistant.say("Error").error(err.code).data(err).finish();
         })
 }
