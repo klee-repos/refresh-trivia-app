b3ca65dd287f11a7bd5efdded68819ea2710f9b6
kevin.lee@refreshlabs.co <kevin.lee@lynkr.xyz>
2018-01-03 19:32:00 -0800

    test

--- server/Intents/NewGame.js
+++ server/Intents/NewGame.js
@@ -3,9 +3,15 @@ var SessionManager = require('../SessionManager');
 const Game = require('../models/Game');
 
 var execute = function(args, assistant){
-    var game = new Game();
-    game.save();
-    assistant.say("Ready to start?").data(game).finish();
+
+    var game = new Game()
+    game.save().then(function() {
+        User.findOne({gAssistantId:args.uniqueUserId}).then(function(user) {
+            user.gameId = game._id
+            user.save()
+            assistant.say("<speak>Ok. Who's on team one?</speak>").finish();
+        })
+    })
 }
 
 var NewGameIntent = {
--- server/models/User.js
+++ server/models/User.js
@@ -6,7 +6,8 @@ var userSchema = new mongoose.Schema(
 	id: mongoose.Schema.Types.ObjectId,
 	began: {type:Date, default:Date.now},
 	sessionCode: String,
-	gAssistantId: String
+	gAssistantId: String,
+	gameId: mongoose.Schema.Types.ObjectId,
 });
 
 var User = mongoose.model('User', userSchema);
