8e8d3c0e968c04fd2aa614064f347eeef25dfb15
kevin.lee@refreshlabs.co <kevin.lee@lynkr.xyz>
2018-01-22 22:21:38 -0800

    completion of about page

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -8,10 +8,15 @@ class About extends Component {
         return (
             <div className='aboutContainer'>
                 <div className='aboutTitle'>
-                <h1>About Refresh Labs</h1>
+                    <h1>Refresh Labs</h1>
                 </div>
                 <div className='brief'>
-                    <p>Founded in 2017, Refresh Labs specializes in building unique and platform agnostic voice and chat applications. </p>
+                    <p>Founded in 2017, Refresh Labs specializes in building platform agnostic conversational applications. </p>
+                    <p>Our in-house tool kit lets us build voice and visual UIs into new and existing applications, that can be invoked from any AI chat provider (i.e Google, Alexa, etc.)</p>
+                </div>
+                <div className="aboutContact">
+                    <h1>Contact Us</h1>
+                    <p>founders@refreshlabs.co</p>
                 </div>
             </div>
         )
