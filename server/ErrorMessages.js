291e77180fac3740eed3d4d6fb6a9ce6d73c3624
Gavin Berkowitz <gavin.berkowitz.bu@gmail.com>
2018-01-31 17:52:41 -0500

    Better welcome

--- server/ErrorMessages.js
+++ server/ErrorMessages.js
@@ -1,6 +1,6 @@
 var Errors = {};
 
-Errors.NeedToConnect = "In order to use this app, visit trivia.refreshlabs.co and connect using the onscreen instructions"
+Errors.NeedToConnect = "This command is not available before connecting. To get a connect code, please visit trivia.refreshlabs.co"
 Errors.NeedATeam = "I didn't get a team name, please try again"
 Errors.NeedPlayers = "I didn't get a list of players, please try again"
 module.exports = Errors;
