fd4493cb8cafd218816c4bd62f210e35f9c4417d
gberk <gberkowitz@logmein.com>
2018-02-01 09:04:24 -0500

    Add roster logging

--- server/Intents/Roster/AddPlayersToTeam.js
+++ server/Intents/Roster/AddPlayersToTeam.js
@@ -10,6 +10,7 @@ var execute = function(args, assistant){
     let user = assistant.deviceProfile.user;
     Game.findById(assistant.deviceProfile.user.game)
         .then(function(game){
+            console.log("Adding " + args.names)
             game.addPlayersToTeam(args.names, args.teamName).then(function(){
                 game.save();
                 SessionManager.sendData(user.sessionCode, 'teamRoster', game.formatRoster());
